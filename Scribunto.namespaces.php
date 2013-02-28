cb09e3e20c8ef23f9f2eaf602241ade7ba687e7c
Liangent <liangent@gmail.com>
2013-02-28 14:50:33 +0800

    Add Chinese namespace names
    Change-Id: I0165d3f9a518334b716f57923346ce743bbdbfcc

--- Scribunto.namespaces.php
+++ Scribunto.namespaces.php
@@ -21,3 +21,13 @@ $namespaceNames['he'] = array(
 	828 => 'יחידה',
 	829 => 'שיחת_יחידה',
 );
+
+$namespaceNames['zh-hans'] = array(
+	828 => '模块',
+	829 => '模块讨论',
+);
+
+$namespaceNames['zh-hant'] = array(
+	828 => '模組',
+	829 => '模組討論',
+);
