5167107324bad3c51b42875d95863419e15b303d
Brad Jorsch <bjorsch@wikimedia.org>
2016-03-14 11:48:20 -0400

    SECURITY: Don't escape strip markers when escaping attributes in mw.html
    Core strip markers were changed in T110143 to include characters that
    are normally encoded in attributes, however we want to pass them through
    here so they can be unstripped correctly in the output wikitext.
    
    This fix makes "Strip markers in CSS" parser test pass again.
    
    Bug: T110143
    Bug: T135961
    Change-Id: I1353931a53c668d8a453dfa2300a99f59fdb01c5

--- engines/LuaCommon/HtmlLibrary.php
+++ engines/LuaCommon/HtmlLibrary.php
@@ -2,6 +2,9 @@
 
 class Scribunto_LuaHtmlLibrary extends Scribunto_LuaLibraryBase {
 	function register() {
-		return $this->getEngine()->registerInterface( 'mw.html.lua', array() );
+		return $this->getEngine()->registerInterface( 'mw.html.lua', array(), array(
+			'uniqPrefix' => Parser::MARKER_PREFIX,
+			'uniqSuffix' => Parser::MARKER_SUFFIX,
+		) );
 	}
 }
