fa7481b6f6fe37da2a4351fd15c60bb381b125d8
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 21:53:22 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: I2aa3c8fa4b04618b7b50e27858cfcb28ce468c66

--- Scribunto.php
+++ Scribunto.php
@@ -6,13 +6,13 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	$wgMessagesDirs['Scribunto'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['ScribuntoMagic'] = __DIR__ . '/Scribunto.magic.php';
 	$wgExtensionMessagesFiles['ScribuntoNamespaces'] = __DIR__ . '/Scribunto.namespaces.php';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for Scribunto extension. Please use wfLoadExtension instead,' .
 		' see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return true;
 } else {
-	die( 'This version of the Scribunto extension requires MediaWiki 1.27+' );
+	die( 'This version of the Scribunto extension requires MediaWiki 1.31+' );
 }
 
 /**
