a55cfc2078900a021875016f8879340c14b0766d
Lucas Werkmeister (WMDE) <lucas.werkmeister@wikimedia.de>
2018-11-30 14:05:55 +0000

    Revert "Adding a unit test for CVE-2014-5461 in Scribunto."
    This test causes spurious CI failures in other projects; to unblock them
    for now, remove the test again until we can figure out how to make it
    work better.
    
    This reverts commit 7a7f5226765e141e29d09772c533af77068a696d.
    
    Bug: T209232
    Change-Id: Id2eeeb781b7a8a6298ba06d78bab238b37dac9ca

--- tests/phpunit/engines/LuaCommon/SecurityTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Scribunto_LuaSecurityTest extends Scribunto_LuaEngineTestBase {
-	protected static $moduleName = 'SecurityTests';
-	protected function getTestModules() {
-		return parent::getTestModules() + [
-			'SecurityTests' => __DIR__ . '/SecurityTests.lua',
-		];
-	}
-}
