wikimedia/mediawiki-extensions-Scribunto

commit a9b073fc6214a03e41262aa605537525b3963cf8

Remove use of Parser::fetchCurrentRevisionOfTitle and Revision::getSha1
Use new fetchCurrentRevisionRecordOfTitle

Bug: T249384
Bug: T251233
Change-Id: I9a4974a990f8e4f7935cd8e38da918fd45a2282c

includes/engines/LuaCommon/TitleLibrary.php
@@ -1,6 +1,7 @@
 <?php
 
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\SlotRecord;
 
 class Scribunto_LuaTitleLibrary extends Scribunto_LuaLibraryBase {
 	// Note these caches are naturally limited to
@@ -286,7 +287,7 @@ class Scribunto_LuaTitleLibrary extends Scribunto_LuaLibraryBase {
 			$title, $title->getArticleID(), $title->getLatestRevID()
 		);
 
-		$rev = $this->getParser()->fetchCurrentRevisionOfTitle( $title );
+		$rev = $this->getParser()->fetchCurrentRevisionRecordOfTitle( $title );
 
 		if ( $title->equals( $this->getTitle() ) ) {
 			$parserOutput = $this->getParser()->getOutput();
@@ -295,7 +296,7 @@ class Scribunto_LuaTitleLibrary extends Scribunto_LuaLibraryBase {
 			wfDebug( __METHOD__ . ": set vary-revision-sha1 for '$title'" );
 		}
 
-		return $rev ? $rev->getContent() : null;
+		return $rev ? $rev->getContent( SlotRecord::MAIN ) : null;
 	}
 
 	/**

commit c062595b01fdc4df1dd76f8dce0ba2e56219dacb

Handle RevisionAccessException with try-catch
Failures caused by a9b073fc6214a03e41262aa605537525b3963cf8

Bug: T252156
Change-Id: I703c2e0b13e79716f156e1c995fd139fbc68e321

includes/engines/LuaCommon/TitleLibrary.php
@@ -1,6 +1,8 @@
 <?php
 
+use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\RevisionAccessException;
 use MediaWiki\Revision\SlotRecord;
 
 class Scribunto_LuaTitleLibrary extends Scribunto_LuaLibraryBase {
@@ -296,7 +298,21 @@ class Scribunto_LuaTitleLibrary extends Scribunto_LuaLibraryBase {
 			wfDebug( __METHOD__ . ": set vary-revision-sha1 for '$title'" );
 		}
 
-		return $rev ? $rev->getContent( SlotRecord::MAIN ) : null;
+		if ( !$rev ) {
+			return null;
+		}
+
+		try {
+			$content = $rev->getContent( SlotRecord::MAIN );
+		} catch ( RevisionAccessException $ex ) {
+			$logger = LoggerFactory::getInstance( 'Scribunto' );
+			$logger->warning(
+				__METHOD__ . ': Unable to transclude revision content',
+				[ 'exception' => $ex ]
+			);
+			$content = null;
+		}
+		return $content;
 	}
 
 	/**