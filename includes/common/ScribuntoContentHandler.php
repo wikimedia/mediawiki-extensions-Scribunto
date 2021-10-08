8c66ac25c9982fe8ff293f6e8406263bc3ee2573
Roman Stolar <roman.stolar@speedandfunction.com>
2021-10-08 13:18:29 +0300

    Override ContentHandler::fillParserOutput instead of Content::fillParserOutput.
    Prepare override for ScribuntoContentHandler, remove usage in ScribuntoContent.
    
    Bug: T287158
    Change-Id: I91f6a077f5cc0de25f83bea26d9f1772af0d3d68

--- includes/common/ScribuntoContentHandler.php
+++ includes/common/ScribuntoContentHandler.php
@@ -1,4 +1,8 @@
 <?php
+
+use MediaWiki\Content\Renderer\ContentParseParams;
+use MediaWiki\MediaWikiServices;
+
 /**
  * Scribunto Content Handler
  *
@@ -51,5 +55,126 @@ class ScribuntoContentHandler extends CodeContentHandler {
 		}
 
 		return parent::canBeUsedOn( $title );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function fillParserOutput(
+		Content $content,
+		ContentParseParams $cpoParams,
+		ParserOutput &$output
+	) {
+		'@phan-var ScribuntoContent $content';
+		$page = $cpoParams->getPage();
+		$title = Title::castFromPageReference( $page );
+		$parserOptions = $cpoParams->getParserOptions();
+		$revId = $cpoParams->getRevId();
+		$generateHtml = $cpoParams->getGenerateHtml();
+		$parser = MediaWikiServices::getInstance()->getParser();
+		$text = $content->getText();
+
+		// Get documentation, if any
+		$output = new ParserOutput();
+		// @phan-suppress-next-line PhanTypeMismatchArgument
+		$doc = Scribunto::getDocPage( $title );
+		if ( $doc ) {
+			$msg = wfMessage(
+				$doc->exists() ? 'scribunto-doc-page-show' : 'scribunto-doc-page-does-not-exist',
+				$doc->getPrefixedText()
+			)->inContentLanguage();
+
+			if ( !$msg->isDisabled() ) {
+				// We need the ParserOutput for categories and such, so we
+				// can't use $msg->parse().
+				$docViewLang = $doc->getPageViewLanguage();
+				$dir = $docViewLang->getDir();
+
+				// Code is forced to be ltr, but the documentation can be rtl.
+				// Correct direction class is needed for correct formatting.
+				// The possible classes are
+				// mw-content-ltr or mw-content-rtl
+				$dirClass = "mw-content-$dir";
+
+				$docWikitext = Html::rawElement(
+					'div',
+					[
+						'lang' => $docViewLang->getHtmlCode(),
+						'dir' => $dir,
+						'class' => $dirClass,
+					],
+					// Line breaks are needed so that wikitext would be
+					// appropriately isolated for correct parsing. See Bug 60664.
+					"\n" . $msg->plain() . "\n"
+				);
+
+				if ( $parserOptions->getTargetLanguage() === null ) {
+					$parserOptions->setTargetLanguage( $doc->getPageLanguage() );
+				}
+				$output = $parser->parse( $docWikitext, $page, $parserOptions, true, true, $revId );
+			}
+
+			// Mark the doc page as a transclusion, so we get purged when it
+			// changes.
+			$output->addTemplate( $doc, $doc->getArticleID(), $doc->getLatestRevID() );
+		}
+
+		// Validate the script, and include an error message and tracking
+		// category if it's invalid
+		// @phan-suppress-next-line PhanTypeMismatchArgument
+		$status = $content->validate( $title );
+		if ( !$status->isOK() ) {
+			$output->setText( $output->getRawText() .
+				Html::rawElement( 'div', [ 'class' => 'errorbox' ],
+					$status->getHTML( 'scribunto-error-short', 'scribunto-error-long' )
+				)
+			);
+			$trackingCategories = MediaWikiServices::getInstance()->getTrackingCategories();
+			$trackingCategories->addTrackingCategory( $output, 'scribunto-module-with-errors-category', $page );
+		}
+
+		if ( !$generateHtml ) {
+			// We don't need the actual HTML
+			$output->setText( '' );
+			return;
+		}
+
+		$engine = Scribunto::newDefaultEngine();
+		// @phan-suppress-next-line PhanTypeMismatchArgument
+		$engine->setTitle( $title );
+		if ( $this->highlight( $text, $output, $engine ) ) {
+			return;
+		}
+
+		// No GeSHi, or GeSHi can't parse it, use plain <pre>
+		$output->setText( $output->getRawText() .
+			"<pre class='mw-code mw-script' dir='ltr'>\n" .
+			htmlspecialchars( $text ) .
+			"\n</pre>\n"
+		);
+	}
+
+	/**
+	 * Adds syntax highlighting to the output (or do not touch it and return false).
+	 * @param string $text
+	 * @param ParserOutput $output
+	 * @param ScribuntoEngineBase $engine
+	 * @return bool Success status
+	 */
+	protected function highlight( $text, ParserOutput $output, ScribuntoEngineBase $engine ) {
+		global $wgScribuntoUseGeSHi;
+		$language = $engine->getGeSHiLanguage();
+		if ( $wgScribuntoUseGeSHi && class_exists( SyntaxHighlight::class ) && $language ) {
+			$status = SyntaxHighlight::highlight( $text, $language, [ 'line' => true, 'linelinks' => 'L' ] );
+			if ( $status->isGood() ) {
+				// @todo replace addModuleStyles line with the appropriate call on
+				// SyntaxHighlight once one is created
+				$output->addModuleStyles( 'ext.pygments' );
+				$output->addModules( 'ext.pygments.linenumbers' );
+				$output->setText( $output->getRawText() . $status->getValue() );
+				return true;
+			}
+		}
+		return false;
 	}
 }
