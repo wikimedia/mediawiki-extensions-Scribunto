c51b6efec12f1d48531fb97ae5351c8e20076c84
Ammarpad <ammarpad@yahoo.com>
2022-02-08 10:33:02 +0000

    Revert "Add test for pageLanguage property"
    This reverts commit 9adf9ef66c4f09124233f5b43529e177df3eebf1.
    
    Reason for revert: The feature it's testing has been reverted in Ic6c0e31c8247f7d89824d20f28fb0aa56d6ed749
    
    Change-Id: I74b4273bf246560b31911f8953974b9653217ec9

--- tests/phpunit/engines/LuaCommon/TitleLibraryTests.lua
+++ tests/phpunit/engines/LuaCommon/TitleLibraryTests.lua
@@ -97,10 +97,6 @@ local function test_getCurrentTitle_fragment()
 	return mw.title.getCurrentTitle().fragment
 end
 
-local function test_pageLanguage()
-	return mw.title.getCurrentTitle().pageLanguage:getCode() == mw.language.getContentLanguage():getCode()
-end
-
 -- Tests
 local tests = {
 	{ name = 'tostring', func = identity, type = 'ToString',
@@ -419,7 +415,6 @@ local tests = {
 	{ name = "fragments don't leak via getCurrentTitle()", func = test_getCurrentTitle_fragment,
 		expect = { '' }
 	},
-	{ name = "mw.pageLanguage", func = test_pageLanguage, expect = { true } },
 }
 
 return testframework.getTestProvider( tests )
