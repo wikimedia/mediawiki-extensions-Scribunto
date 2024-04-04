dfdf5cb54a4385ec70f383188f0de1a262d8fdae
C. Scott Ananian <cscott@cscott.net>
2024-04-04 10:11:54 -0400

    LanguageLibrary: Ensure language code is a string
    Defense in depth: catch the error early when calling mw.language.new()
    with a non-string `code` argument; but also check late when popping
    the code off the argument array in case something altered the code
    property after construction.
    
    Bug: T361664
    Change-Id: I04476d3e1c32c87e1df95cc9a0dddcaff475b756

--- includes/Engines/LuaCommon/LanguageLibrary.php
+++ includes/Engines/LuaCommon/LanguageLibrary.php
@@ -186,8 +186,12 @@ class LanguageLibrary extends LibraryBase {
 	 * @return array
 	 * @throws LuaError
 	 */
-	public function languageMethod( $name, $args ) {
-		$name = strval( $name );
+	public function languageMethod( string $name, array $args ): array {
+		if ( !is_string( $args[0] ?? null ) ) {
+			throw new LuaError(
+				"invalid code property of language object when calling $name"
+			);
+		}
 		$code = array_shift( $args );
 		if ( !isset( $this->langCache[$code] ) ) {
 			if ( count( $this->langCache ) > $this->maxLangCacheSize ) {
