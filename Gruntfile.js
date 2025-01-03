1fb0328bd9a91c675b0f00f294c0bbaea03a23b0
Ed Sanders <esanders@wikimedia.org>
2025-01-03 12:29:13 +0000

    eslint: Use server config for server files
    Also ignore files using .eslintignore
    
    Change-Id: I9c4cfce821e1700e3fc19d61610d1e2438268646

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 'use strict';
-/* eslint-env node, es6 */
+
 module.exports = function ( grunt ) {
 	const conf = grunt.file.readJSON( 'extension.json' );
 
@@ -12,10 +12,7 @@ module.exports = function ( grunt ) {
 			options: {
 				cache: true
 			},
-			all: [
-				'**/*.js{,on}',
-				'!{vendor,node_modules}/**'
-			]
+			all: [ '.' ]
 		},
 		banana: conf.MessagesDirs,
 		stylelint: {
