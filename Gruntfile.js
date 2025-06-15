0b52c3f935523678262d52ede53c44d87da115f5
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2025-06-15 17:45:48 +0000

    build: Updating brace-expansion to 1.1.12, 2.0.2
    * https://github.com/advisories/GHSA-v6h2-p8h4-qcjw
    
    Additional changes:
    * Enable stylelint caching.
    
    Change-Id: I862eae8c1f4d0498a5bcb54d59bd047bc28b7704

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,9 @@ module.exports = function ( grunt ) {
 		},
 		banana: conf.MessagesDirs,
 		stylelint: {
+			options: {
+				cache: true
+			},
 			all: [
 				'**/*.{css,less}',
 				'!{vendor,node_modules}/**'
